2ded75e5aef1cf09e53175ee50aca7873f4dad3c
myrinja <myrinjau98@gmail.com>
2023-06-19 13:11:08 -0700

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - prints the name of the file
  *
- * Returns:Always (0)success
+ * Return: Always 0 (Success)
  */
-
-int main() {
-	printf("Compiled from file: %s\n", __FILE__);
-	return 0;
+int ain(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
 
