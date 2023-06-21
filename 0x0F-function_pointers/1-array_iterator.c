032e8b1884e2d75f62e7a13ab862b143e5ab42f9
myrinja <myrinjau98@gmail.com>
2023-06-21 13:15:26 -0700

    update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,16 +1,21 @@
 #include "function_pointers.h"
-#include <stdio.h>
 /**
- * print_name - print name using pointer to function
- * @name: string to add
- * @f: pointer to function
- * Return: nothing
- **/
-void print_name(char *name, void (*f)(char *))
+ * array_iterator - this functions iterates an array
+(* a blank line
+ *@array: the array to iterate
+ *@size: the size of the array.
+ *@action: this a function pointer.
+* Description: this function iterate an array)?
+(* section header: the header of this function is function_pointers.h)*
+* Return: this is a void function no return
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (name == NULL || f == NULL)
-		return;
+	size_t i;
 
-	f(name);
+	if (array && size && action)
+	{
+	for (i = 0; i < size; i++)
+		action(*(array + i));
+	}
 }
-
